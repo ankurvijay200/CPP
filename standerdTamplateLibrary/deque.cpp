2a0b5e051e8d617d5b7bd98be0f0d2b02568783c
ankurvijay200 <ankurvijay200@gmail.com>
2023-01-04 01:14:37 +0530

    deque commit

--- standerdTamplateLibrary/deque.cpp
+++ standerdTamplateLibrary/deque.cpp
@@ -3,5 +3,34 @@
 using namespace std;
 
 int main(){
-    
+    deque<int> d;
+
+    d.push_back(10);
+    d.push_front(20);
+    d.push_front(30);
+    d.push_back(5);
+
+    for(int i:d){
+        cout<<i<<" ";
+    }
+
+    cout<<endl<<"Size of deque -->"<<d.size();
+
+    cout<<endl<<"Element at 2nd index -->"<<d.at(2);
+
+    cout<<endl<<"Front -->>"<<d.front()<<" Back -->"<<d.back();
+
+    cout<<endl<<"isEmpty -->"<<d.empty();
+
+    cout<<endl<<"Before erase -->";
+    for(int i:d){
+        cout<<i<<" ";
+    }
+
+    d.erase(d.begin(), d.begin()+1);
+
+    cout<<endl<<"After erase -->";
+    for(int i:d){
+        cout<<i<<" ";
+    }
 }

c8736b8b1a87bc1eaeb5964cb80e2002710a8e2c
ankurvijay200 <ankurvijay200@gmail.com>
2023-01-05 04:31:31 +0530

    optimized changes commit

--- array/algo/questions/mergeSortedArray.cpp
+++ array/algo/questions/mergeSortedArray.cpp
@@ -5,12 +5,10 @@ using namespace std;
 void mergeSortedArray(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3, int m, int n){
     int i = 0, j = 0;
     while(i<m && j<n){
-        if(arr1[i] < arr2[j]){
+        if(arr1[i] < arr2[j])
             arr3.push_back(arr1[i++]);
-        }
-        else{
+        else
             arr3.push_back(arr2[j++]);
-        }
     }
 
     while(i<m){

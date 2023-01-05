f120f80d4696126782a9d335a71dd12428e1de67
ankurvijay200 <ankurvijay200@gmail.com>
2023-01-05 23:50:10 +0530

    commit changes

--- second.cpp
+++ second.cpp
@@ -2,23 +2,5 @@
 using namespace std;
 
 int main(){
-    int rangeStart, rangeEnd;
-    cin>>rangeStart>>rangeEnd;
-
-    for (int n = rangeStart; n < rangeEnd; n++)
-    {
-        int num = n, digit, rev = 0;
-
-    while (num != 0)
-    {
-        digit = num % 10;
-        rev = ( rev * 10) + digit;
-        num = num / 10;
-    }
-    if (rev ==  n)
-    {
-        cout<<n<<" ";
-    }
-    }
-    return 0;
+    
 }
